f2d59df40653a77fad15ff2613056f7252658807
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-03-09 07:11:45 +0300

    [09-03-07-11] fixed providers

--- src/main/java/by/vorakh/training/my_finance/controller/impl/LoginProvider.java
+++ src/main/java/by/vorakh/training/my_finance/controller/impl/LoginProvider.java
@@ -11,74 +11,26 @@ import by.vorakh.training.my_finance.controller.command.impl.Exit;
 import by.vorakh.training.my_finance.controller.command.impl.SignIn;
 import by.vorakh.training.my_finance.controller.command.impl.SignUp;
 import by.vorakh.training.my_finance.controller.command.impl.WrongRequest;
-import by.vorakh.training.my_finance.convertor.impl.AccountToStringConvertor;
-import by.vorakh.training.my_finance.convertor.impl.ExpenseRecordToStringConvertor;
 import by.vorakh.training.my_finance.convertor.impl.LoginResponseStringConvertor;
 import by.vorakh.training.my_finance.convertor.impl.RequestToUserConvertor;
-import by.vorakh.training.my_finance.convertor.impl.StringToAccountConvertor;
-import by.vorakh.training.my_finance.convertor.impl.StringToExpenseRecordConvertor;
-import by.vorakh.training.my_finance.convertor.impl.StringToUserConvertor;
-import by.vorakh.training.my_finance.convertor.impl.UserToStringConvertor;
-import by.vorakh.training.my_finance.dao.builder.DAOBuilder;
-import by.vorakh.training.my_finance.dao.datasource.builder.DataSourceBuilder;
-import by.vorakh.training.my_finance.dao.datasource.impl.AccountDataSourceImpl;
-import by.vorakh.training.my_finance.dao.datasource.impl.ExpenseDataSourceImpl;
-import by.vorakh.training.my_finance.dao.datasource.impl.UserDataSourceImpl;
-import by.vorakh.training.my_finance.dao.impl.FileAccountDAO;
-import by.vorakh.training.my_finance.dao.impl.FileExpenseRecordDAO;
-import by.vorakh.training.my_finance.dao.impl.FileUserDAO;
-import by.vorakh.training.my_finance.service.builder.ServiceBuilder;
-import by.vorakh.training.my_finance.service.impl.AccountServiceImpl;
-import by.vorakh.training.my_finance.service.impl.ExpenseRecordServiceImpl;
-import by.vorakh.training.my_finance.service.impl.UserServiceImpl;
+import by.vorakh.training.my_finance.service.factory.ServiceFactory;
 
 public class LoginProvider implements CommandProvider {
 
     private final Map<CommandName, Command> repository =
             new EnumMap<>(CommandName.class);
 
-    private ServiceBuilder serviceBuilder;
-    private DAOBuilder daoBuilder;
-    private DataSourceBuilder dataSourceBuilder;
-
     LoginProvider() {
-        this.dataSourceBuilder = new DataSourceBuilder();
-        dataSourceBuilder.setAccountDataSource(
-                new AccountDataSourceImpl(new StringToAccountConvertor(),
-                new AccountToStringConvertor()));
-        dataSourceBuilder.setExpenseRecordDataSource(new ExpenseDataSourceImpl(
-                new StringToExpenseRecordConvertor(),
-                new ExpenseRecordToStringConvertor()));
-        dataSourceBuilder.setUserDataSourse(new UserDataSourceImpl(
-                new StringToUserConvertor(), new UserToStringConvertor()));
-        this.daoBuilder = new DAOBuilder();
-        daoBuilder.setExpenseRecordDAO(new FileExpenseRecordDAO(
-                dataSourceBuilder.getExpenseRecordDataSource(),
-                dataSourceBuilder.getAccountDataSource()));
-        daoBuilder.setAccountDAO(new FileAccountDAO(
-                dataSourceBuilder.getAccountDataSource(),
-                daoBuilder.getExpenseRecordDAO()));
-        daoBuilder.setUserDAO(new FileUserDAO(
-                dataSourceBuilder.getUserDataSourse(),
-                daoBuilder.getAccountDAO()));
-        this.serviceBuilder = new ServiceBuilder();
-        serviceBuilder.setExpenseRecordService(new ExpenseRecordServiceImpl(
-                daoBuilder.getAccountDAO(), daoBuilder.getExpenseRecordDAO()));
-        serviceBuilder.setAccountService(new AccountServiceImpl(
-                daoBuilder.getAccountDAO(), daoBuilder.getUserDAO(),
-                daoBuilder.getExpenseRecordDAO()));
-        serviceBuilder.setUserService(new UserServiceImpl(
-                daoBuilder.getUserDAO(), serviceBuilder.getAccountService()));
         init();
     }
 
     private void init() {
         repository.put(CommandName.SIGN_IN, new SignIn(
-                serviceBuilder.getUserService(),
+                new ServiceFactory().getUserService(),
                 new  LoginResponseStringConvertor(),
                 new RequestToUserConvertor()));
         repository.put(CommandName.SIGN_UP, new SignUp(
-                serviceBuilder.getUserService(),
+                new ServiceFactory().getUserService(),
                 new  LoginResponseStringConvertor(),
                 new RequestToUserConvertor()));
         repository.put(CommandName.WRONG_REQUEST, new WrongRequest());
--- src/main/java/by/vorakh/training/my_finance/controller/impl/UserCommandProvider.java
+++ src/main/java/by/vorakh/training/my_finance/controller/impl/UserCommandProvider.java
@@ -17,65 +17,18 @@ import by.vorakh.training.my_finance.controller.command.impl.MyExpenses;
 import by.vorakh.training.my_finance.controller.command.impl.SelectAccount;
 import by.vorakh.training.my_finance.controller.command.impl.SelectExpense;
 import by.vorakh.training.my_finance.controller.command.impl.WrongRequest;
-import by.vorakh.training.my_finance.convertor.impl.AccountToStringConvertor;
-import by.vorakh.training.my_finance.convertor.impl.ExpenseRecordToStringConvertor;
 import by.vorakh.training.my_finance.convertor.impl.RequestToAccountConvertor;
 import by.vorakh.training.my_finance.convertor.impl.RequestToExpenseRecordConvertor;
 import by.vorakh.training.my_finance.convertor.impl.RequestToIdConvertor;
-import by.vorakh.training.my_finance.convertor.impl.StringToAccountConvertor;
-import by.vorakh.training.my_finance.convertor.impl.StringToExpenseRecordConvertor;
-import by.vorakh.training.my_finance.convertor.impl.StringToUserConvertor;
-import by.vorakh.training.my_finance.convertor.impl.UserToStringConvertor;
-import by.vorakh.training.my_finance.dao.builder.DAOBuilder;
-import by.vorakh.training.my_finance.dao.datasource.builder.DataSourceBuilder;
-import by.vorakh.training.my_finance.dao.datasource.impl.AccountDataSourceImpl;
-import by.vorakh.training.my_finance.dao.datasource.impl.ExpenseDataSourceImpl;
-import by.vorakh.training.my_finance.dao.datasource.impl.UserDataSourceImpl;
-import by.vorakh.training.my_finance.dao.impl.FileAccountDAO;
-import by.vorakh.training.my_finance.dao.impl.FileExpenseRecordDAO;
-import by.vorakh.training.my_finance.dao.impl.FileUserDAO;
-import by.vorakh.training.my_finance.service.builder.ServiceBuilder;
-import by.vorakh.training.my_finance.service.impl.AccountServiceImpl;
-import by.vorakh.training.my_finance.service.impl.ExpenseRecordServiceImpl;
-import by.vorakh.training.my_finance.service.impl.UserServiceImpl;
+import by.vorakh.training.my_finance.service.factory.ServiceFactory;
+
 
 public class UserCommandProvider implements CommandProvider {
 	
 	private final Map<CommandName, Command> repository = 
 			new EnumMap<>(CommandName.class);
 	
-	private ServiceBuilder serviceBuilder;
-	private DAOBuilder daoBuilder;
-	private DataSourceBuilder dataSourceBuilder;
-	
 	UserCommandProvider(){
-		this.dataSourceBuilder = new DataSourceBuilder();
-		dataSourceBuilder.setAccountDataSource(
-		new AccountDataSourceImpl(new StringToAccountConvertor(),
-				new AccountToStringConvertor()));
-		dataSourceBuilder.setExpenseRecordDataSource(new ExpenseDataSourceImpl(
-				new StringToExpenseRecordConvertor(), 
-				new ExpenseRecordToStringConvertor()));
-		dataSourceBuilder.setUserDataSourse(new UserDataSourceImpl
-				(new StringToUserConvertor(), new UserToStringConvertor()));
-		this.daoBuilder = new DAOBuilder();
-		daoBuilder.setExpenseRecordDAO(new FileExpenseRecordDAO(
-				dataSourceBuilder.getExpenseRecordDataSource(), 
-				dataSourceBuilder.getAccountDataSource()));
-		daoBuilder.setAccountDAO(new FileAccountDAO(
-				dataSourceBuilder.getAccountDataSource(), 
-				daoBuilder.getExpenseRecordDAO()));
-		daoBuilder.setUserDAO(new FileUserDAO(
-				dataSourceBuilder.getUserDataSourse(), 
-				daoBuilder.getAccountDAO()));
-		this.serviceBuilder = new ServiceBuilder();
-		serviceBuilder.setExpenseRecordService(new ExpenseRecordServiceImpl(
-				daoBuilder.getAccountDAO(), daoBuilder.getExpenseRecordDAO()));
-        serviceBuilder.setAccountService(new AccountServiceImpl(
-                daoBuilder.getAccountDAO(), daoBuilder.getUserDAO(),
-                daoBuilder.getExpenseRecordDAO()));
-		serviceBuilder.setUserService(new UserServiceImpl(
-				daoBuilder.getUserDAO(), serviceBuilder.getAccountService()));
 		init();
 	}
 
@@ -83,29 +36,29 @@ public class UserCommandProvider implements CommandProvider {
 		repository.put(CommandName.WRONG_REQUEST, new WrongRequest());
 		repository.put(CommandName.EXIT, new Exit());
 		repository.put(CommandName.MY_ACCOUNTS, new MyAccounts(
-				serviceBuilder.getAccountService(), 
+		        new ServiceFactory().getAccountService(), 
 				new RequestToIdConvertor()));
 		repository.put(CommandName.CREATE_ACCOUNT, new CreateAccount(
 				new RequestToAccountConvertor(), new RequestToIdConvertor(),
-				serviceBuilder.getAccountService()));
+				new ServiceFactory().getAccountService()));
 		repository.put(CommandName.SELECT_ACCOUNT, new SelectAccount(
 				new RequestToIdConvertor(), 
-				serviceBuilder.getAccountService()));
+				new ServiceFactory().getAccountService()));
 		repository.put(CommandName.DELETE_ACCOUNT, new DeleteAccount(
-		        serviceBuilder.getAccountService(), 
+		        new ServiceFactory().getAccountService(), 
 		        new RequestToIdConvertor()));
 		repository.put(CommandName.MY_EXPENSES, new MyExpenses(
-		        serviceBuilder.getExpenseRecordService(), 
+		        new ServiceFactory().getExpenseRecordService(), 
 		        new RequestToIdConvertor()));
 		repository.put(CommandName.SELECT_EXPENSE, new SelectExpense(
-		        serviceBuilder.getExpenseRecordService(), 
+		        new ServiceFactory().getExpenseRecordService(), 
                 new RequestToIdConvertor()));
 		repository.put(CommandName.CREATE_EXPENSE, new CreateExpense(
-		        serviceBuilder.getExpenseRecordService(), 
+		        new ServiceFactory().getExpenseRecordService(), 
                 new RequestToIdConvertor(),
 		        new RequestToExpenseRecordConvertor()));
 		repository.put(CommandName.DELETE_EXPENSE, new DeleteExpense(
-		        serviceBuilder.getExpenseRecordService(), 
+		        new ServiceFactory().getExpenseRecordService(), 
                 new RequestToIdConvertor()));
 	}
 	
