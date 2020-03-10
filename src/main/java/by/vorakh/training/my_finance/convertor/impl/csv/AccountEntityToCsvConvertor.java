6c131feedbc998f3cd8dc149357314858491ceaf
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-03-10 12:39:16 +0300

    added AccountEntity To Csv Convertor with its test

---
+++ src/main/java/by/vorakh/training/my_finance/convertor/impl/csv/AccountEntityToCsvConvertor.java
@@ -0,0 +1,25 @@
+package by.vorakh.training.my_finance.convertor.impl.csv;
+
+import static by.vorakh.training.my_finance.convertor.exception.ConvertorException.PROBLEM;
+
+import by.vorakh.training.my_finance.convertor.Convertor;
+import by.vorakh.training.my_finance.convertor.exception.ConvertorException;
+import by.vorakh.training.my_finance.dao.entity.AccountEntity;
+import by.vorakh.training.my_finance.validation.dao_entity.AccountEntityValidator;
+
+public class AccountEntityToCsvConvertor implements AccountEntityValidator, 
+        Convertor< AccountEntity, String> {
+
+    @Override
+    public String converte(AccountEntity object) throws ConvertorException {
+        if (!isCorrectEntity(object)) {
+            String message = "[AccountEntityToCsvConvertor]" + PROBLEM  
+                    + "AccountEntity has null value or One and more fields "
+                    + "have null value.";
+            throw new ConvertorException(message);
+        }
+        return String.format("%s,%s,%s", object.getId(), 
+                object.getName(), object.getBalance());
+    }
+
+}
