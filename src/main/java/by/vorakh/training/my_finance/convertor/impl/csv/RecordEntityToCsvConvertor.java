dbf017ac22d49d43d630cc35700e1c4f90828fd0
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-03-10 15:47:51 +0300

    added Record entity to csv convertor and its test

---
+++ src/main/java/by/vorakh/training/my_finance/convertor/impl/csv/RecordEntityToCsvConvertor.java
@@ -0,0 +1,25 @@
+package by.vorakh.training.my_finance.convertor.impl.csv;
+
+import static by.vorakh.training.my_finance.convertor.exception.ConvertorException.PROBLEM;
+
+import by.vorakh.training.my_finance.convertor.Convertor;
+import by.vorakh.training.my_finance.convertor.exception.ConvertorException;
+import by.vorakh.training.my_finance.dao.entity.RecordEntity;
+import by.vorakh.training.my_finance.validation.dao_entity.RecordEntityValidator;
+
+public class RecordEntityToCsvConvertor implements RecordEntityValidator, 
+        Convertor<RecordEntity, String> {
+
+    @Override
+    public String converte(RecordEntity object) throws ConvertorException {
+        if (!isCorrectEntity(object)) {
+            String message = "[RecordEntityToCsvConvertor]" + PROBLEM  
+                    + "RecordEntity has null value or One and more fields "
+                    + "have null value.";
+            throw new ConvertorException(message);
+        }
+        return String.format("%s,%s,%s", object.getId(), 
+                object.getAmount(), object.getType());
+    }
+
+}
