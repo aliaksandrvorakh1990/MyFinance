f744e0a44a8063bdc393ae8c7d9ff47083899eed
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-03-11 14:59:57 +0300

    Renamed method getRecordDAO in DaoFactory

--- src/main/java/by/vorakh/training/my_finance/dao/factory/DaoFactory.java
+++ src/main/java/by/vorakh/training/my_finance/dao/factory/DaoFactory.java
@@ -100,7 +100,7 @@ public class DaoFactory {
         return accountDAO;
     }
 
-    public RecordDAO getExpenseRecordDAO() {
+    public RecordDAO getRecordDAO() {
         return recordDAO;
     }
 
