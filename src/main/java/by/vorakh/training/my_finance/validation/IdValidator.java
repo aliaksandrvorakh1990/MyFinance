a866d9db49d400f27f9b8b6b7c59b10173404bc7
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-03-11 11:53:35 +0300

    fixed IdValidator for csv

--- src/main/java/by/vorakh/training/my_finance/validation/IdValidator.java
+++ src/main/java/by/vorakh/training/my_finance/validation/IdValidator.java
@@ -3,12 +3,12 @@ package by.vorakh.training.my_finance.validation;
 public interface IdValidator extends NotNullValidator {
 
     default boolean isAccountId(String str) {
-        String regex = "-?\\d+@\\d+";
+        String regex = "^[a-zA-Z][a-zA-Z\\d]{3,19}?@\\d+";
         return ((!isEqualsNull(str)) && (str.matches(regex)));
     }
 
-    default boolean isExpenseRecordId(String str) {
-        String regex = "-?\\d+@\\d+T\\d+";
+    default boolean isRecordId(String str) {
+        String regex = "^[a-zA-Z][a-zA-Z\\d]{3,19}?@\\d+@\\d+";
         return ((!isEqualsNull(str)) && (str.matches(regex)));
     }
 }
