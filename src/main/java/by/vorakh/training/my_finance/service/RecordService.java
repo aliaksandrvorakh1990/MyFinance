1e1e7df4c75b07896ba0eb17765aee145752f065
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-03-11 11:58:52 +0300

    renamed and fixed RecordService

--- src/main/java/by/vorakh/training/my_finance/service/RecordService.java
+++ src/main/java/by/vorakh/training/my_finance/service/RecordService.java
@@ -6,7 +6,7 @@ import by.vorakh.training.my_finance.bean.Record;
 import by.vorakh.training.my_finance.bean.ExpenseType;
 import by.vorakh.training.my_finance.service.exception.ServiceException;
 
-public interface ExpenseRecordService extends Service<Record, String> {
+public interface RecordService extends Service<Record, String> {
     
     List<Record> getAll(String  accountId) throws ServiceException ;
 
