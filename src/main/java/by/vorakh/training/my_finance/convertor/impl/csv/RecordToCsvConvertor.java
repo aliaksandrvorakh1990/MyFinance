3ae7af4057565a30d58b70af51e186f9c984fe84
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2020-03-16 18:38:36 +0300

    Renamed RecordToCSVConvertor and its tests

--- src/main/java/by/vorakh/training/my_finance/convertor/impl/csv/RecordToCsvConvertor.java
+++ src/main/java/by/vorakh/training/my_finance/convertor/impl/csv/RecordToCsvConvertor.java
@@ -1,17 +1,17 @@
 package by.vorakh.training.my_finance.convertor.impl.csv;
 
-import static by.vorakh.training.my_finance.validation.dao_entity.RecordEntityValidator.isCorrectEntity;
+import static by.vorakh.training.my_finance.validation.bean.RecordValidator.isCorrectForConverting;
 
+import by.vorakh.training.my_finance.bean.Record;
 import by.vorakh.training.my_finance.convertor.Convertor;
 import by.vorakh.training.my_finance.convertor.exception.ConvertorException;
-import by.vorakh.training.my_finance.dao.entity.RecordEntity;
 
-public class RecordEntityToCsvConvertor implements
-        Convertor<RecordEntity, String> {
+public class RecordToCsvConvertor implements
+        Convertor<Record, String> {
 
     @Override
-    public String converte(RecordEntity object) {
-        if (!isCorrectEntity(object)) {
+    public String converte(Record object) {
+        if (!isCorrectForConverting(object)) {
             String message = "RecordEntity has null value or One and more fields "
                     + "have null value.";
             throw new ConvertorException(message);
--- src/test/java/by/vorakh/training/my_finance/convertor/impl/csv/RecordToCsvConvertorTest.java
+++ src/test/java/by/vorakh/training/my_finance/convertor/impl/csv/RecordToCsvConvertorTest.java
@@ -1,16 +1,17 @@
 package by.vorakh.training.my_finance.convertor.impl.csv;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.math.BigDecimal;
 
 import org.junit.Test;
 
 import by.vorakh.training.my_finance.bean.ExpenseType;
+import by.vorakh.training.my_finance.bean.Record;
 import by.vorakh.training.my_finance.convertor.exception.ConvertorException;
-import by.vorakh.training.my_finance.dao.entity.RecordEntity;
 
-public class RecordEntityToCsvConvertorTest {
+public class RecordToCsvConvertorTest {
 
     @Test
     public void testConverte_Correct_Record() throws ConvertorException {
@@ -19,8 +20,8 @@ public class RecordEntityToCsvConvertorTest {
         BigDecimal amount = new BigDecimal(123.24)
                 .setScale(2, BigDecimal.ROUND_HALF_UP);
         ExpenseType type = ExpenseType.CAR;
-        RecordEntity record = new RecordEntity(id, amount, type);
-        RecordEntityToCsvConvertor convertor = new RecordEntityToCsvConvertor();
+        Record record = new Record(id, amount, type);
+        RecordToCsvConvertor convertor = new RecordToCsvConvertor();
         String actual = convertor.converte(record);
         assertEquals(expected, actual);
     }
@@ -28,8 +29,8 @@ public class RecordEntityToCsvConvertorTest {
     @Test(expected = ConvertorException.class)
     public void testConverte_Null_Record_ThrownException() throws 
             ConvertorException {
-        RecordEntity record = null;
-        RecordEntityToCsvConvertor convertor = new RecordEntityToCsvConvertor();
+        Record record = null;
+        RecordToCsvConvertor convertor = new RecordToCsvConvertor();
         String actual = convertor.converte(record);
         assertNull(actual);
     }
